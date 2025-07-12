1f765f0d41da05c708aa7614778709bce8a11653
kushagra <agrakush4@gmail.com>
2025-07-12 22:54:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Video-conference-site/', 
+  base: '/Video-conference-site/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
 })
