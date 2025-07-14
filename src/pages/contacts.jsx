84943f57e6b8503cb862a4f62b2b982b3e914ea1
argahsuk <agrakush4@gmail.com>
2025-07-15 01:48:46 +0530

    Your commit message

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,54 +1,49 @@
-import React from 'react';
-
 function Contacts() {
   return (
-    <div className="contacts-wrapper">
-      <h1 className="contacts-heading">Connect with Me</h1>
-      <div className="card-container">
+    <div className="card-container">
 
-        <a href="https://www.linkedin.com/in/kushagra512" target="_blank" rel="noopener noreferrer">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" className="card-image" />
-            <h2 className="card-title">LinkedIn</h2>
-          </div>
-        </a>
+      <a href="https://www.linkedin.com/in/kushagra512" target="_blank" rel="noopener noreferrer">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" className="card-image" />
+          <h2 className="card-title">LinkedIn</h2>
+        </div>
+      </a>
 
-        <a href="https://github.com/argahsuk" target="_blank" rel="noopener noreferrer">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" className="card-image" />
-            <h2 className="card-title">GitHub</h2>
-          </div>
-        </a>
+      <a href="https://github.com/argahsuk" target="_blank" rel="noopener noreferrer">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" className="card-image" />
+          <h2 className="card-title">Git-Hub</h2>
+        </div>
+      </a>
 
-        <a href="mailto:kushagra.dd24.cs@nitp.ac.in">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email" className="card-image" />
-            <h2 className="card-title">E-mail</h2>
-          </div>
-        </a>
+      <a href="mailto:kushagra.dd24.cs@nitp.ac.in">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email" className="card-image" />
+          <h2 className="card-title">E-mail</h2>
+        </div>
+      </a>
 
-        <a href="https://www.instagram.com/_argahsuk" target="_blank" rel="noopener noreferrer">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" className="card-image" />
-            <h2 className="card-title">Instagram</h2>
-          </div>
-        </a>
+      <a href="https://www.instagram.com/_argahsuk" target="_blank" rel="noopener noreferrer">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" className="card-image" />
+          <h2 className="card-title">Instagram</h2>
+        </div>
+      </a>
 
-        <a href="https://t.me/Argahsuk" target="_blank" rel="noopener noreferrer">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111646.png" alt="Telegram" className="card-image" />
-            <h2 className="card-title">Telegram</h2>
-          </div>
-        </a>
+      <a href="https://t.me/Argahsuk" target="_blank" rel="noopener noreferrer">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/2111/2111646.png" alt="Telegram" className="card-image" />
+          <h2 className="card-title">Telegram</h2>
+        </div>
+      </a>
 
-        <a href="https://x.com/agrakush4" target="_blank" rel="noopener noreferrer">
-          <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968830.png" alt="X / Twitter" className="card-image" />
-            <h2 className="card-title">Twitter/X</h2>
-          </div>
-        </a>
+      <a href="https://x.com/agrakush4" target="_blank" rel="noopener noreferrer">
+        <div className="card">
+          <img src="https://cdn-icons-png.flaticon.com/512/5968/5968830.png" alt="X / Twitter" className="card-image" />
+          <h2 className="card-title">Twitter/X</h2>
+        </div>
+      </a>
 
-      </div>
     </div>
   );
 }
